cd0333ec5b20b5548f2a324616c518b10d1ffc9c
Alice Wyan <alice@finitud.org>
2015-11-06 13:32:03 +0000

    Implement user balances

--- lib/hypothesis/ledger.rb
+++ lib/hypothesis/ledger.rb
@@ -3,21 +3,29 @@ module Hypothesis
     attr_accessor :users
     attr_accessor :transactions
 
+    UserTransaction = Struct.new(:date, :counterparty, :amount)
+
     def initialize
       @users ||= {}
       @transactions ||= []
     end
 
     def add_user(user_name)
-      @users.merge!(user_name => 0.0)
+      @users.merge!(user_name => [])
     end
 
     def balance_for(user, date: nil)
-      @users.fetch(user)
+      running_total = 0.0
+      @users.fetch(user).each do |t|
+        running_total += t.amount
+      end
+      running_total
     end
 
-    def add_transaction(transaction)
-      @transactions << transaction
+    def add_transaction(date, user, counterparty, amount)
+      @transactions << [date, user, counterparty, amount]
+      @users[user] << UserTransaction.new(date, counterparty, -amount.to_f)
+      @users[counterparty] << UserTransaction.new(date, user, amount.to_f)
     end
   end
 end
--- spec/ledger_spec.rb
+++ spec/ledger_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe Hypothesis::Ledger do
 
   context 'User creation' do
     describe '#add_user' do
-      it 'creates a new user with a 0.0 balance' do
-        expect do
-          ledger.add_user("new user")
-        end.to change { ledger.users.count }.by(1)
+      before do
+        ledger.add_user("new user")
+      end
 
+      it 'creates a new user with a 0.0 balance and no transactions' do
+        expect(ledger.users.count).to eq(1)
         expect(ledger.users).to include("new user")
+
+        expect(ledger.users["new user"].count).to eq(0)
         expect(ledger.balance_for("new user")).to eq(0.0)
       end
     end
@@ -21,16 +24,25 @@ RSpec.describe Hypothesis::Ledger do
     before do
       ledger.add_user("alice")
       ledger.add_user("bob")
+      ledger.add_transaction("2015-01-16", "alice", "bob", "10.00")
     end
 
     describe '#add_transaction' do
       it 'adds a new transaction to the ledger' do
-        transaction = [ "2015-01-16", "alice", "bob", "10.00" ]
-
-        expect do
-          ledger.add_transaction(transaction)
-        end.to change { ledger.transactions.count }.by(1)
+        expect(ledger.transactions.count).to eq(1)
       end
+
+      it 'adds a new transaction to each of the two counterparties' do
+        expect(ledger.users["alice"].count).to eq(1)
+        expect(ledger.users["bob"].count).to eq(1)
+      end
+
+      it 'updates user balances' do
+        expect(ledger.balance_for("alice")).to eq(-10.0)
+        expect(ledger.balance_for("bob")).to eq(10.0)
+      end
+    end
+
   end
 
 end
