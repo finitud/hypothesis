b61fb0244d6b7f94f7df378455349e53f27c724d
Alice Wyan <alice@finitud.org>
2015-11-06 11:43:52 +0000

    Explore ledger interface

--- run.rb
+++ run.rb
@@ -1,1 +1,15 @@
 require 'hypothesis'
+require 'csv'
+
+ledger = Hypothesis::Ledger.new
+
+%w(john mary supermarket).each do |user|
+  ledger.add_user(user)
+end
+
+CSV.read('example-data.csv').each do |transaction|
+  ledger.add_transaction(transaction)
+end
+
+puts ledger.balance(user: "mary")
+puts ledger.balance(user: "mary", date: "2015-01-16")
